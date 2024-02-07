d3d5bcaabb69a4c7c20883f437dbea55eadd9a9c
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2024-02-07 21:12:12 +0100

    startingCodePanel

--- javascript/login.js
+++ javascript/login.js
@@ -1,14 +1,14 @@
 const submit = document.getElementById("btn-submit");
 
-submit.addEventListener("click",(e) => {
+submit.addEventListener("click", (e) => {
   e.preventDefault();
   let email = document.getElementById("userEmail").value;
   let pwd = document.getElementById("userPwd").value;
   let user = JSON.parse(localStorage.getItem(email));
   console.log(user.pwd);
   console.log(pwd);
-  if(user != null){
-    if(user.pwd == pwd) {
+  if (user != null) {
+    if (user.pwd == pwd) {
       sessionStorage.setItem("userLogged", JSON.stringify(user));
     } else {
       console.log("not the same pwd");
--- javascript/register.js
+++ javascript/register.js
@@ -1,18 +1,18 @@
 const submit = document.getElementById("btn-submit");
 
-submit.addEventListener("click",(e) => {
+submit.addEventListener("click", (e) => {
   e.preventDefault();
   let name = document.getElementById("userName").value;
   let email = document.getElementById("userEmail").value;
   let pwd = document.getElementById("userPwd").value;
   let user = {
-    name:name,
-    email:email,
-    pwd:pwd,
-    games:{
-      game1:{},
-      game2:{}
-    }
+    name: name,
+    email: email,
+    pwd: pwd,
+    games: {
+      game1: {},
+      game2: {},
+    },
   };
   localStorage.setItem(email, JSON.stringify(user));
 });
